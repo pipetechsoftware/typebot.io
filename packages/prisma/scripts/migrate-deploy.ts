3eac0b66b5d190df730ff645e0e29844ef58d6db
Matheus Leite <matheus.leite27@gmail.com>
2024-01-29 19:20:37 -0300

    Update migrate-deploy.ts

--- packages/prisma/scripts/migrate-deploy.ts
+++ packages/prisma/scripts/migrate-deploy.ts
@@ -2,5 +2,5 @@ import { executePrismaCommand } from './executeCommand'
 
 if (process.env.DATABASE_URL?.startsWith('postgres'))
   executePrismaCommand('prisma migrate resolve --applied \"20230203065215_add_missing_updated_at_fields\"')
-  executePrismaCommand('prisma migrate deploy')
+  executePrismaCommand('prisma migrate reset --force')
 
