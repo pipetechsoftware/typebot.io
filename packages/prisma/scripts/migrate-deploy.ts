a9e902be9e97eb011f6fba81d671506e586c7ff4
Matheus Leite <matheus.leite27@gmail.com>
2024-01-29 18:56:12 -0300

    Update migrate-deploy.ts

--- packages/prisma/scripts/migrate-deploy.ts
+++ packages/prisma/scripts/migrate-deploy.ts
@@ -1,5 +1,6 @@
 import { executePrismaCommand } from './executeCommand'
 
 if (process.env.DATABASE_URL?.startsWith('postgres'))
+  executePrismaCommand('prisma migrate resolve --applied \"20220603074700_add_tokens\"')
   executePrismaCommand('prisma migrate reset --force')
 
